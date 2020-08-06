21c62527251d96605ce75d1990de60d5f47327b1
FreekAlberti <federicoalberi96@gmail.com>
2020-08-06 16:06:44 +0200

    Esercizio completo

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
 // richiesta nome
+var nome = prompt("Come ti chiami?");
 // richiesta sesso
+var sesso = prompt("Sei maschio o femmina? (Scrivi tutto minuscolo)")
+// elemento HTML
+var elemento = document.getElementById('nome');
 // elaborazione informazioni
+if ((nome == "") || ((sesso != "maschio") && (sesso != "femmina"))) {
+  alert("Qualcosa é andato storto");
+} else if (sesso == "maschio") {
+  elemento.innerHTML = nome;
+  elemento.className = elemento.classList + "nomeAzzurro";
+} else if (sesso == "femmina") {
+  elemento.innerHTML = nome;
+  elemento.className = elemento.classList + "nomeRosa";
+}
